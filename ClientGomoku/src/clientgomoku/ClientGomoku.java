32606acbea64e6fdc61b1aa1859b5205a14ac4b3
SatriaPriambada <satriapriambada@gmail.com>
2015-12-01 15:57:57 +0700

    creating foundation

--- ClientGomoku/src/clientgomoku/ClientGomoku.java
+++ ClientGomoku/src/clientgomoku/ClientGomoku.java
@@ -8,82 +8,432 @@
  * Connection learned from https://www.youtube.com/watch?v=_1ThWf9Fkfo
  */
 package clientgomoku;
+//
+//import java.io.DataInputStream;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import static java.lang.System.exit;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.JTable;
+import javax.swing.JApplet;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-/**
- *
- * @author Satria
- */
-public class ClientGomoku {
-    static Socket socket;
-    static DataInputStream in;
-    static DataOutputStream out;
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception{
-        //
-        System.out.println("Starting client input IPadress and socket of server");
-        Scanner sc = new Scanner(System.in);
-        String IPServer = sc.nextLine();
-        String SocketServer = sc.nextLine();
-        System.out.println("Connecting to server...");      
-        socket = new Socket(IPServer,Integer.valueOf(SocketServer));
-        System.out.println("Connection sucessfull.");
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        Input input = new Input (in);
-        Thread thread = new Thread(input);
-        thread.start();
-        
-        while(true){
-            String sendMessage = sc.nextLine();
-            out.writeUTF(sendMessage);
-        }
-        
-    }
-    
-}
+//import java.io.DataOutputStream;
+//import java.io.IOException;
+//import java.net.Socket;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import javafx.application.Application;
+//import javafx.geometry.Pos;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.input.MouseButton;
+//import javafx.scene.layout.Pane;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.paint.Color;
+//import javafx.scene.shape.Rectangle;
+//import javafx.scene.text.Font;
+//import javafx.scene.text.Text;
+//import javafx.stage.Stage;
+//import javax.swing.JFrame;
+//import javax.swing.JTable;
+//
+///**
+// *
+// * @author Satria
+// */
+//public class ClientGomoku extends Application{
+//    static Socket socket;
+//    static DataInputStream in;
+//    static DataOutputStream out;
+//    
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) throws Exception{
+//        //
+//        System.out.println("Starting client input IPadress and socket of server");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("input IP adress");
+//        String IPServer = sc.nextLine();
+//        System.out.println("input port number");
+//        String SocketServer = sc.nextLine();
+//        System.out.println("Connecting to server...");      
+//        socket = new Socket(IPServer,Integer.valueOf(SocketServer));
+//        System.out.println("Connection sucessfull.");
+//        in = new DataInputStream(socket.getInputStream());
+//        out = new DataOutputStream(socket.getOutputStream());
+//        Input input = new Input (in);
+//        Thread thread = new Thread(input);
+//        thread.start();
+//        boolean runOnce = true;
+//        while(true){
+//            if (runOnce){
+//                launch(args);
+//                runOnce = false;
+//            }
+//            
+//            String sendMessage = sc.nextLine();
+//            out.writeUTF(sendMessage);
+//            
+//        }
+//        
+//        
+//    }
+//    private class Tile extends StackPane {
+//        private Text text = new Text();
+//
+//        public Tile() {
+//            Rectangle border = new Rectangle(30, 30);
+//            border.setFill(null);
+//            border.setStroke(Color.BLACK);
+//
+//            text.setFont(Font.font(30));
+//
+//            setAlignment(Pos.CENTER);
+//            getChildren().addAll(border, text);
+//
+//            setOnMouseClicked(event -> {
+//                if (!playable)
+//                    return;
+//
+//                if (event.getButton() == MouseButton.PRIMARY) {
+//                    if (!turnX)
+//                        return;
+//
+//                    drawX();
+//                    turnX = false;
+//                    System.out.println("coord X: "+ this.getCenterX());
+//                    System.out.println("coord Y: "+ this.getCenterY());
+////                    checkState();
+//                }
+//                else if (event.getButton() == MouseButton.SECONDARY) {
+//                    if (turnX)
+//                        return;
+//
+//                    drawO();
+//                    turnX = true;
+////                    checkState();
+//                }
+//            });
+//        }
+//
+//        public double getCenterX() {
+//            return getTranslateX() + 15;
+//        }
+//
+//        public double getCenterY() {
+//            return getTranslateY() + 15;
+//        }
+//
+//        public String getValue() {
+//            return text.getText();
+//        }
+//
+//        private void drawX() {
+//            text.setText("X");
+//        }
+//
+//        private void drawO() {
+//            text.setText("O");
+//        }
+//    }
+//    private boolean playable = true;
+//    private boolean turnX = true;
+//    private Tile[][] board = new Tile[20][20];
+////    private List<SimpleBoard.Combo> combos = new ArrayList<>();
+//
+//    private Pane root = new Pane();
+//
+//    private Parent createContent() {
+//        root.setPrefSize(800, 1000);
+//
+//        for (int i = 0; i < 20; i++) {
+//            for (int j = 0; j < 20; j++) {
+//                Tile tile = new Tile();
+//                tile.setTranslateX(j * 30);
+//                tile.setTranslateY(i * 30);
+//
+//                root.getChildren().add(tile);
+//
+//                board[i][j] = tile;
+//            }
+//        }
+////
+////        // horizontal
+////        for (int y = 0; y < 3; y++) {
+////            combos.add(new SimpleBoard.Combo(board[0][y], board[1][y], board[2][y]));
+////        }
+////
+////        // vertical
+////        for (int x = 0; x < 3; x++) {
+////            combos.add(new SimpleBoard.Combo(board[x][0], board[x][1], board[x][2]));
+////        }
+////
+////        // diagonals
+////        combos.add(new SimpleBoard.Combo(board[0][0], board[1][1], board[2][2]));
+////        combos.add(new SimpleBoard.Combo(board[2][0], board[1][1], board[0][2]));
+//
+//        return root;
+//    }
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        primaryStage.setScene(new Scene(createContent()));
+//        primaryStage.show();
+//    }
+//    
+//}
+//
+//class Input implements Runnable{
+//    DataInputStream in;
+//    //constructor default do nothing
+//    //constructor with parameter
+//    public Input(DataInputStream in){
+//        this.in = in;
+//    }
+//    
+//    @Override
+//    public void run(){
+//        while(true){
+//            try {
+//                String message = in.readUTF();
+//                System.out.println(message);
+//            } catch (IOException ex) {
+//                Logger.getLogger(Input.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//    }
+//}
 
-class Input implements Runnable{
-    DataInputStream in;
-    //constructor default do nothing
-    //constructor with parameter
-    public Input(DataInputStream in){
-        this.in = in;
-    }
-    
-    public void run(){
-        while(true){
-            try {
-                String message = in.readUTF();
-                System.out.println(message);
-            } catch (IOException ex) {
-                Logger.getLogger(Input.class.getName()).log(Level.SEVERE, null, ex);
+// Client class to let a user play Tic-Tac-Toe with
+// another user across a network.
+public class ClientGomoku extends JApplet
+                             implements Runnable {
+   private JTextField id;
+   private JTextArea display;
+   private JPanel boardPanel, panel2;
+   private Square board[][], currentSquare;
+   private Socket connection;
+   private DataInputStream input;
+   private DataOutputStream output;
+   private Thread outputThread;
+   private char myMark;
+   private boolean myTurn;
+ 
+   // Set up user-interface and board
+   public void init()
+   {
+      display = new JTextArea( 4, 30 );
+      display.setEditable( false );
+      getContentPane().add( new JScrollPane( display ),
+                            BorderLayout.SOUTH );
+ 
+      boardPanel = new JPanel();
+      GridLayout layout = new GridLayout( 20, 20, 0, 0 );
+      boardPanel.setLayout( layout );
+ 
+      board = new Square[ 20 ][ 20 ];
+ 
+      // When creating a Square, the location argument to the
+      // constructor is a value from 0 to 8 indicating the
+      // position of the Square on the board. Values 0, 1,
+      // and 2 are the first row, values 3, 4, and 5 are the
+      // second row. Values 6, 7, and 8 are the third row.
+      for ( int row = 0; row < board.length; row++ )
+      {
+         for ( int col = 0;col < board[ row ].length; col++ ) {
+            board[ row ][ col ] =
+               new Square( ' ', row * 20 + col );
+            board[ row ][ col ].addMouseListener((MouseListener) new SquareListener(
+                  this, board[ row ][ col ] ));
+ 
+            boardPanel.add( board[ row ][ col ] );        
+         }
+      }
+ 
+      id = new JTextField();
+      id.setEditable( false );
+       
+      getContentPane().add( id, BorderLayout.NORTH );
+       
+      panel2 = new JPanel();
+      panel2.add( boardPanel, BorderLayout.CENTER );
+      getContentPane().add( panel2, BorderLayout.CENTER );
+   }
+ 
+   // Make connection to server and get associated streams.
+   // Start separate thread to allow this applet to
+   // continually update its output in text area display.
+   public void start()
+   {
+      try {
+         connection = new Socket(
+            InetAddress.getByName( "127.0.0.1" ), 5000 );
+         input = new DataInputStream(
+                        connection.getInputStream() );
+         output = new DataOutputStream(
+                        connection.getOutputStream() );
+      }
+      catch ( IOException e ) {
+         e.printStackTrace();         
+      }
+ 
+      outputThread = new Thread( this );
+      outputThread.start();
+   }
+ 
+   // Control thread that allows continuous update of the
+   // text area display.
+   public void run()
+   {
+      // First get player's mark (X or O)
+      try {
+         myMark = input.readChar();
+         id.setText( "You are player \"" + myMark + "\"" );
+         if( myMark == 'X' ){
+             myTurn = true;
+         }else if( myMark == 'O' ){
+             myTurn = false;
+         }else if( myMark == '#' ){
+             myTurn = false;
+         } 
+      }
+      catch ( IOException e ) {
+         e.printStackTrace();         
+      }
+ 
+      // Receive messages sent to client
+      while ( true ) {
+         try {
+            String s = input.readUTF();
+            processMessage( s );
+         }
+         catch ( IOException e ) {
+            e.printStackTrace();         
+         }
+      }
+   }
+ 
+   // Process messages sent to client
+   public void processMessage( String s )
+   {
+      if ( s.equals( "Valid move." ) ) {
+         display.append( "Valid move, please wait.\n" );
+         currentSquare.setMark( myMark );
+         currentSquare.repaint();
+      }
+      else if ( s.equals( "Invalid move, try again" ) ) {
+         display.append( s + "\n" );
+         myTurn = true;
+      }
+      else if ( s.equals( "Opponent moved" ) ) {
+         try {
+            int loc = input.readInt();
+            char movingPlayer = input.readChar();
+            if (movingPlayer == 'X'){
+                board[ loc / 20 ][ loc % 20 ].setMark('X');
+            }else if (movingPlayer == 'O'){
+                board[ loc / 20 ][ loc % 20 ].setMark('O');
+            }else if (movingPlayer == '#'){
+                board[ loc / 20 ][ loc % 20 ].setMark('#');
             }
-        }
-    }
+            
+            board[ loc / 20 ][ loc % 20 ].repaint();
+                  
+            display.append(
+               "Opponent moved. Your turn.\n" );
+            myTurn = true;
+         }
+         catch ( IOException e ) {
+            e.printStackTrace();         
+         }
+      }
+      else
+         display.append( s + "\n" );
+ 
+      display.setCaretPosition(
+         display.getText().length() );
+   }
+ 
+   public void sendClickedSquare( int loc )
+   {
+      if ( myTurn )
+         try {
+            output.writeInt( loc );
+            myTurn = false;
+         }
+         catch ( IOException ie ) {
+            ie.printStackTrace();         
+         }
+   }
+ 
+   public void setCurrentSquare( Square s )
+   {
+      currentSquare = s;
+   }
 }
-
-class GomokuTable extends JFrame{
-    JTable table;
-    
-    public GomokuTable(){
-        setLayout(new FlowLayout());
-        
-        String[] columnNames = {"1"};
-        table = new JTable(10,10);
-    }
+ 
+// Maintains one square on the board
+class Square extends JPanel {
+   private char mark;
+   private int location;
+ 
+   public Square( char m, int loc)
+   {
+      mark = m;
+      location = loc;
+      setSize ( 25, 25 );
+       
+      setVisible(true);
+   }
+ 
+   public Dimension getPreferredSize() { 
+      return ( new Dimension( 25, 25 ) );
+   }
+ 
+   public Dimension getMinimumSize() {
+      return ( getPreferredSize() );
+   }
+ 
+   public void setMark( char c ) { mark = c; }
+ 
+   public int getSquareLocation() { return location; }
+ 
+   public void paintComponent( Graphics g )
+   {
+      super.paintComponent( g );
+      g.drawRect( 0, 0, 25, 25 );
+      g.drawString( String.valueOf( mark ), 11, 20 );   
+   }
+}
+ 
+class SquareListener extends MouseAdapter {
+   private ClientGomoku applet;
+   private Square square;
+ 
+   public SquareListener( ClientGomoku t, Square s )
+   {
+      applet = t;
+      square = s;
+   }
+ 
+   public void mouseReleased( MouseEvent e )
+   {
+      applet.setCurrentSquare( square );
+      applet.sendClickedSquare( square.getSquareLocation() );
+   }
 }
--- ServerGomoku/src/servergomoku/ServerGomoku.java
+++ ServerGomoku/src/servergomoku/ServerGomoku.java
@@ -6,93 +6,257 @@
  * You are free to use and distribute the code
  * We do not take responsibilities for any damage caused by using this code
  * Connection learned from https://www.youtube.com/watch?v=_1ThWf9Fkfo
+ * http://www.java-tips.org/java-se-tips-100019/15-javax-swing/584-a-game-of-tic-tac-toe-that-can-be-played-between-two-client-applets.html
  */
 package servergomoku;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author Satria
- */
-public class ServerGomoku {
-    static ServerSocket serverSocket;
-    static Socket socket;
-    static DataOutputStream out;
-    static DataInputStream in;
-    static int MaxUser = 5;
-    static Users[] user = new Users[MaxUser];
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        System.out.println("Starting server gomoku...");
-        serverSocket = new ServerSocket(60301);
-        System.out.println("Server started ...");
-        while(true){
-            //accepts any connection coming to the socket
-            socket = serverSocket.accept();
-            //Check for max user
-            for (int i =0; i < MaxUser; i++){
-                //check which sender send it
-                System.out.println("Connection from: "+ socket.getInetAddress());
-                //get input and output stream from clients
-                out = new DataOutputStream(socket.getOutputStream());
-                in = new DataInputStream(socket.getInputStream());
-                //Check is it existing user or new user
-                if (user[i] == null){
-                    user[i] = new Users(out,in,user);
-                    Thread thread = new Thread(user[i]);
-                    //start the new client
-                    thread.start();
-                    break;
-                }
-            }
-            
-        }
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+ 
+public class ServerGomoku extends JFrame {
+ 
+   private byte board[];
+   private boolean xMove;
+   private JTextArea output;
+   private Player players[];
+   private ServerSocket server;
+   private int currentPlayer;
+ 
+   public ServerGomoku()
+   {
+      super( "Gomoku Server" );
+ 
+      board = new byte[ 400 ];
+      xMove = true;
+      players = new Player[ 3 ];
+      currentPlayer = 0;
+  
+      // set up ServerSocket
+      try {
+         server = new ServerSocket( 5000, 3 );
+      }
+      catch( IOException e ) {
+         e.printStackTrace();
+         System.exit( 1 );
+      }
+ 
+      output = new JTextArea();
+      getContentPane().add( output, BorderLayout.CENTER );
+      output.setText( "Server awaiting connections\n" );
+ 
+      setSize( 300, 300 );
+      show();
+   }
+ 
+   // wait for two connections so game can be played
+   public void execute()
+   {
+      for ( int i = 0; i < players.length; i++ ) {
+         try {
+            players[ i ] =
+               new Player( server.accept(), this, i );
+            players[ i ].start();
+         }
+         catch( IOException e ) {
+            e.printStackTrace();
+            System.exit( 1 );
+         }
+      }
+ 
+      // Player X is suspended until Player O connects.
+      // Resume player X now.          
+      synchronized ( players[ 0 ] ) {
+         players[ 0 ].threadSuspended = false;   
+         players[ 0 ].notify();
+      }
+   
+   }
+    
+   public void display( String s )
+   {
+      output.append( s + "\n" );
+   }
+  
+   // Determine if a move is valid.
+   // This method is synchronized because only one move can be
+   // made at a time.
+   public synchronized boolean validMove( int loc,
+                                          int player )
+   {
+      boolean moveDone = false;
+ 
+      while ( player != currentPlayer ) {
+         try {
+            wait();
+         }
+         catch( InterruptedException e ) {
+            e.printStackTrace();
+         }
+      }
+      char movingPlayer = 'a';
+      if ( !isOccupied( loc ) ) {
+         if (currentPlayer == 0){
+            board[ loc ] =(byte) ('X');
+            movingPlayer = 'X';
+         }else if (currentPlayer == 1){
+            board[ loc ] =(byte) ('0');
+            movingPlayer = 'O';
+         }else if (currentPlayer == 2){
+            board[ loc ] =(byte) ('#');
+            movingPlayer = '#';
+         }
         
-    }
-    
-}
-
-class Users implements Runnable{
-    private DataOutputStream out;
-    private DataInputStream in;
-    private int MaxUser = 5;
-    Users[] user = new Users[MaxUser];
-    String name;
-    //constructor default do nothing
-    //constructor with parameter
-    public Users(DataOutputStream out, DataInputStream in, Users[] user){
-        this.out = out;
-        this.in = in;
-        this.user = user;
-    }
-    public void run(){
-        try {
-            name = in.readUTF();
-        } catch (IOException ex) {
-            Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        while (true){
-            try {
-                String message = in.readUTF();
-                for(int i = 0; i < MaxUser; i++){
-                    if (user[i] != null){
-                        user[i].out.writeUTF(name + ":"+ message);
-                    }
-                    //else user is not connected yet so user ==null
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
+          for (int i = 0; i < players.length; i++) {
+              players[i].otherPlayerMoved(loc,movingPlayer);
+          }
+         currentPlayer = ( currentPlayer + 1 ) % 3;
+         //players[ currentPlayer ].otherPlayerMoved( loc );
+         notify();    // tell waiting player to continue
+         return true;
+      }
+      else
+         return false;
+   }
+ 
+   public boolean isOccupied( int loc )
+   {
+      if ( board[ loc ] == 'X' || board [ loc ] == 'O' || board [ loc ] == '#')
+          return true;
+      else
+          return false;
+   }
+ 
+   public boolean gameOver()
+   {
+      // Place code here to test for a winner of the game
+      return false;
+   }
+ 
+   public static void main( String args[] )
+   {
+      ServerGomoku game = new ServerGomoku();
+ 
+      game.addWindowListener( new WindowAdapter() {
+        public void windowClosing( WindowEvent e )
+            {
+               System.exit( 0 );
             }
-        }
-    }
-    
+         }
+      );
+ 
+      game.execute();
+   }
 }
+ 
+// Player class to manage each Player as a thread
+class Player extends Thread {
+   private Socket connection;
+   private DataInputStream input;
+   private DataOutputStream output;
+   private ServerGomoku control;
+   private int number;
+   private char mark;
+   protected boolean threadSuspended = true;
+ 
+   public Player( Socket s, ServerGomoku t, int num )
+   {
+      if ( num == 0 )
+          mark = 'X';
+      else if (num == 1)
+          mark = 'O';
+      else if (num == 2)
+          mark = '#';
+ 
+      connection = s;
+       
+      try {
+         input = new DataInputStream(
+                    connection.getInputStream() );
+         output = new DataOutputStream(
+                    connection.getOutputStream() );
+      }
+      catch( IOException e ) {
+         e.printStackTrace();
+         System.exit( 1 );
+      }
+ 
+      control = t;
+      number = num;
+   }
+ 
+   public void otherPlayerMoved( int loc , char movingPlayer)
+   {
+      try {
+         output.writeUTF( "Opponent moved" );
+         output.writeInt( loc );
+         output.writeChar(movingPlayer);
+      }
+      catch ( IOException e ) { e.printStackTrace(); }
+   }
+ 
+   public void run()
+   {
+      boolean done = false;
+ 
+      try {
+         if (number == 0){
+            control.display( "Player " + 'X' + " connected" );
+         } else if (number == 1){
+            control.display( "Player " + '0' + " connected" );
+         } if (number == 2){
+            control.display( "Player " + '#' + " connected" );
+         }
+         output.writeChar( mark );
+         if (number == 0){
+            output.writeUTF( "Player " + 'X' + " connected\n" );
+         } else if (number == 1){
+            output.writeUTF( "Player " + '0' + " connected\n" );
+         } if (number == 2){
+            output.writeUTF( "Player " + '#' + " connected. Please wait..\n" );
+         }
+ 
+         // wait for another player to arrive
+         if ( mark == 'X' ) {
+            output.writeUTF( "Waiting for another player" );
+ 
+            try {
+               synchronized( this ) {   
+                  while ( threadSuspended )
+                     wait();  
+               }
+            } 
+            catch ( InterruptedException e ) {
+               e.printStackTrace();
+            }
+ 
+            output.writeUTF(
+               "Other player connected. Your move." );
+         }
+ 
+         // Play game
+         while ( !done ) {
+            int location = input.readInt();
+ 
+            if ( control.validMove( location, number ) ) {
+               control.display( "loc: " + location );
+               output.writeUTF( "Valid move." );
+            }
+            else
+               output.writeUTF( "Invalid move, try again" );
+ 
+            if ( control.gameOver() )
+               done = true;
+         }         
+ 
+         connection.close();
+      }
+      catch( IOException e ) {
+         e.printStackTrace();
+         System.exit( 1 );
+      }
+   }
+}                                                         
+ 
